d2c76776f7793ac122bdc7285fa70a8fba34ebd9
FranciscoJSB12 <javif8@gmail.com>
2024-05-09 10:40:26 -0400

    feat: Implement automatic, light or dark theme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { PaperProvider } from 'react-native-paper';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { ThemeContextProvider } from './src/presentation/context/ThemeContext';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeContextProvider>
+      <StackNavigator />
+    </ThemeContextProvider>
   );
 }
